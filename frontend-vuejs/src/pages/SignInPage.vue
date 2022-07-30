db9e5cd01a63d703a2e29f723a1b14bf69f5eab5
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-07-30 12:05:53 -0300

    Vue signin ui

--- frontend-vuejs/src/pages/SignInPage.vue
+++ frontend-vuejs/src/pages/SignInPage.vue
@@ -2,20 +2,45 @@
 import { defineComponent } from "vue"
 
 export default defineComponent({
-  name: "SignInPage"
+  name: "SignInPage",
+  data: () => ({
+    isSubmitted: false,
+    email: "",
+    password: ""
+  }),
+  methods: {
+    handleSubmit(e: any) {
+      this.isSubmitted = true
+      e.preventDefault()
+      // Submit api call
+      this.email = ""
+      this.password = ""
+      setTimeout(() => { this.isSubmitted = false }, 2000)
+    }
+  }
 })
 </script>
 
 <template>
   <div class="container">
     <section class="heading">
-      <h1 class="page-title">Sign In</h1>
+      <h1 class="page-title">
+        <i class="fa-solid fa-right-to-bracket"></i>
+        Sign In
+      </h1>
+      <p>Inform your credentials to access the dashboard</p>
     </section>
     <section class="form">
-      <form>
+      <form @submit="handleSubmit">
         <div class="form-group">
           <label class="label-block">E-mail</label>
-          <input type="email" v-model="email" class="form-text" placeholder="E-mail address registred" required />
+          <input
+            type="email"
+            v-model="email"
+            class="form-text"
+            placeholder="E-mail address registred"
+            required
+          />
         </div>
         <div class="form-group">
           <label class="label-block">Password</label>
@@ -29,7 +54,7 @@ export default defineComponent({
           />
         </div>
         <div class="form-group">
-          <button type="submit" class="btn">Submit</button>
+          <button type="submit" class="btn" :disabled="isSubmitted && 'disabled'">Submit</button>
         </div>
       </form>
     </section>
